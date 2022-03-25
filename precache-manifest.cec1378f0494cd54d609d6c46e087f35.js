45e8b15f02150018450ebd0b7ed9ecfad89e25de
Saran-73 <saransamikannu@gmail.com>
2022-03-25 18:57:54 +0530

    Updates

---
+++ precache-manifest.cec1378f0494cd54d609d6c46e087f35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "336040a56d0041c479ab",
+    "url": "/Random-advice-generator-React-app/static/css/main.5a8a05b4.chunk.css"
+  },
+  {
+    "revision": "336040a56d0041c479ab",
+    "url": "/Random-advice-generator-React-app/static/js/main.336040a5.chunk.js"
+  },
+  {
+    "revision": "a3c0bc647fa7f281b58f",
+    "url": "/Random-advice-generator-React-app/static/js/1.a3c0bc64.chunk.js"
+  },
+  {
+    "revision": "36139aa1baf21ee4db16",
+    "url": "/Random-advice-generator-React-app/static/js/runtime~main.36139aa1.js"
+  },
+  {
+    "revision": "a03cc98132c921ae91d2bb2b16aea5d2",
+    "url": "/Random-advice-generator-React-app/static/media/pattern-divider-desktop.a03cc981.svg"
+  },
+  {
+    "revision": "afbe147e3feefda674d629dd1e8b288d",
+    "url": "/Random-advice-generator-React-app/static/media/pattern-divider-mobile.afbe147e.svg"
+  },
+  {
+    "revision": "1c39833246687c42894218820a8458d5",
+    "url": "/Random-advice-generator-React-app/index.html"
+  }
+];
