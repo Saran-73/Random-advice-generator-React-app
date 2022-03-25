9354555a26f36006625516e554fb1ff375bc7a78
Saran-73 <saransamikannu@gmail.com>
2022-03-25 19:12:29 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function App(){
+
+  const [id,setId]=useState(0)
+  const [advice,setAdvice]=useState('')
+  const [count,setCount]=useState(0)
+
+useEffect(()=>{
+        fetch("https://api.adviceslip.com/advice")
+        .then(res=>res.json())
+         .then(x=>{
+           setId(x.slip.id)
+           setAdvice(x.slip.advice)
+          })
+  },[count])
+
+  return (
+    <main>
+    <div className="card">
+    <h1 className="advice--id">ADVICE #{id}</h1>
+    <p>"{advice}"</p>
+    <div className="image"></div>
+    <div onClick={()=>setCount(x=>x+1)} className="choose"><img src="../icon-dice.svg" /></div>
+    </div>
+    </main>
+  )
+}
